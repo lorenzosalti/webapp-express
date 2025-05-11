6c5ffe0af4ce5974be485e3781a547411d0ad4d4
lorenzosalti <lorenzosalti@gmail.com>
2025-05-11 17:36:07 +0200

    feat: middlewares import in app

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+// middlewares import
+const genericError = require('./middlewares/genericError.js');
+const notFound = require('./middlewares/notFound.js');
 
 
 // homepage
 app.get('/', (req, res) => {
   res.send('Webapp homepage');
 });
+
+
+
+// middleware for handling errors
+app.use(genericError);
+app.use(notFound);
 
 
 // server listen
