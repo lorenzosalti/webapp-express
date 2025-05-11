a41310d8a00940b043970984eeca561b4d1a1441
lorenzosalti <lorenzosalti@gmail.com>
2025-05-11 18:52:39 +0200

    feat: use .env data in the project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // routers import
 const moviesRouter = require('./routers/movies.js');
--- data/db.js
+++ data/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // connection to db
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: ''
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 // db error handling
