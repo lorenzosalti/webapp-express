effe6f446cda6b54d45f81fb603887ca2417961b
lorenzosalti <lorenzosalti@gmail.com>
2025-05-11 17:52:38 +0200

    add: router with index and show routes

---
+++ routers/movies.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const movieController = require('../controllers/movieController.js');
+
+
+// index
+router.get('/', movieController.index);
+
+// show
+router.get('/:id', movieController.show);
+
+
+
+module.exports = router;
