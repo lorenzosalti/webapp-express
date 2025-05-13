58a42ff89a9ddc21f86aaf0337e756896a5ae6e2
lorenzosalti <lorenzosalti@gmail.com>
2025-05-13 15:37:07 +0200

    fix: sql injection

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -5,19 +5,25 @@ function index(req, res) {
 
   const { search } = req.query;
 
+  const queryParams = [];
+
   // sql query
   let sql = `
     SELECT movies.*, ROUND(AVG(reviews.vote), 2) AS average_vote
     FROM movies
     LEFT JOIN reviews
-    ON movies.id = reviews.movie_id`
+    ON movies.id = reviews.movie_id`;
 
-  if (search) sql += ` WHERE title LIKE "%${search}%" OR director LIKE "%${search}%" OR abstract LIKE "%${search}%"`
+  if (search) {
+    queryParams.push(`%${search}%`, `%${search}%`, `%${search}%`);
+
+    sql += ` WHERE title LIKE ? OR director LIKE ? OR abstract LIKE ?`;
+  }
 
   sql += ` GROUP BY movies.id`;
 
   // connection to db
-  connection.query(sql, (err, results) => {
+  connection.query(sql, queryParams, (err, results) => {
 
     if (err) return res.status(500).json({ error: 'Database query failed' });
 
@@ -58,12 +64,12 @@ function show(req, res) {
     connection.query(sql, [movieId], (err, results) => {
 
       // db or query fail
-      if (err) console.log(err)
+      if (err) console.log(err);
 
       // create reviews key in movie
       movie.reviews = results;
 
-      movie.image = process.env.PUBLIC_PATH + movie.image
+      movie.image = process.env.PUBLIC_PATH + movie.image;
 
       res.json(movie);
 
