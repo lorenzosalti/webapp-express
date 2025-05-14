35e737fb00afa0294ba6f7f5e3a8cac3882c27a4
lorenzosalti <lorenzosalti@gmail.com>
2025-05-14 15:18:14 +0200

    add: route to store reviews

--- routers/movies.js
+++ routers/movies.js
@@ -9,6 +9,9 @@ router.get('/', movieController.index);
 // show
 router.get('/:id', movieController.show);
 
+// store reviews
+router.post('/:id/reviews', movieController.storeReview);
+
 
 
 module.exports = router;
